8f6aaa16615403347010a5a1beb456337c711145
nakhalanatqiyaAP <nakhalanatqiya@gmail.com>
2025-12-03 19:32:20 +0700

    Minggu 9: Array 2 Dimensi

---
+++ pertemuan8/array2dimensi/latihan.cpp
@@ -0,0 +1,36 @@
+#include <iostream>
+
+using namespace std;
+
+int main()
+{
+  int bil[3][3];
+  int input_1, input_2;
+
+  cout<<"=== Latihan Array 2 Dimensi ==="<<endl;
+
+  cout<<"Masukan kolom (dengan pola index): ";
+  cin>>input_1;
+  cout<<"Masukan baris (dengan pola index): ";
+  cin>>input_2;
+
+  for (int i = 0; i <input_1 ; i++)
+  {
+   for (int n = 0; n < input_2; n++)
+   {
+  printf("Masukan data index [%d][%d] : ", i, n);
+  cin>>bil[i][n];
+   }
+  }
+  for (int i = 0; i < input_1; i++)
+  {
+    for (int n = 0; n < input_2; n++)
+    {
+     cout<<bil[i][n]<<" ";
+    }
+    cout<<endl;
+  }
+  
+  
+  return 0;
+}
---
+++ pertemuan8/array3dimensi/latihan.cpp
@@ -0,0 +1,12 @@
+#include <iostream>
+
+using namespace std;
+
+int main()
+{
+  int bil[2][3][4]{
+    {{3,5,6,5},{5,10,2},{21,9,4,2}},
+    {{2,4,12,2},{2,3,2},{21,9,4,2}},
+  };
+  return 0;
+}
