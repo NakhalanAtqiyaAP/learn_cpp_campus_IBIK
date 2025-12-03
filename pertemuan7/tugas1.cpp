8686e656c4bc640964e0f203b96264744442339a
nakhalanatqiyaAP <nakhalanatqiya@gmail.com>
2025-12-03 18:46:14 +0700

    pertemuan 8 : Function Strings

---
+++ pertemuan7/tugas1.cpp
@@ -0,0 +1,11 @@
+#include <iostream>
+#include <string.h>
+using namespace std;
+
+int main()
+{
+  char name[100] = "FAKULTAS PARAWISATA DAN TEKNOLOGI INFORMASI";
+
+  cout<<"Panjang kata: "<<strlen(name)<<endl;
+  return 0;
+}
---
+++ pertemuan7/tugas3.cpp
@@ -0,0 +1,13 @@
+#include <iostream>
+
+using namespace std;
+
+int main()
+{
+  string text1 = "Teknik";
+    string text2 = "Informatika";
+      string result = text1 + text2;
+
+      cout<<result<<endl;
+  return 0;
+}
