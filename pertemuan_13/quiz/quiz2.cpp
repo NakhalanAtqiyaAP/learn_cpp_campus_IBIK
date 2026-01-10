1c90247ac7d36f747d0921ceb3fedf5f714d6121
NakhalanAtqiya <nakhalanatqiya@gmail.com>
2026-01-10 21:33:17 +0700

    quiz 2 & quiz 3

--- pertemuan_13/quiz/quiz2.cpp
+++ pertemuan_13/quiz/quiz2.cpp
@@ -1,8 +1,31 @@
 #include <iostream>
 using namespace std;
 
+void validation(int *bilangan){
+    if (*bilangan < 0)
+    {
+        cout<<"bilangan tidak valid, karena negatif"<<endl;
+        *bilangan = (*bilangan - *bilangan) + 1;
+        cout<<"angka valid terdekat: "<<*bilangan;
+    }else if(*bilangan > 100){
+        cout<<"bilangan tidak valid, lebih dari 100"<<endl;
+        *bilangan = (*bilangan - *bilangan) + 99;
+        cout<<"angka valid terdekat: "<<*bilangan;
+    }else{
+        cout<<"Angka Valid ";
+    }
+    
+}
+
+
 int main()
 {
-    cout<<"hello";
+    int bil;
+    cout<<"Masukan inputan berupa 0-100 :";
+    cin>>bil;
+
+    validation(&bil);
+
+    cout<<"\nAngka dari main : "<<bil;
     return 0;
 }
---
+++ pertemuan_13/quiz/quiz3.cpp
@@ -0,0 +1,39 @@
+#include <iostream>
+using namespace std;
+
+struct Pegawai
+{
+    string nama;
+    string jabatan;
+    int gaji;
+};
+
+int umr(Pegawai pgw){
+    if (pgw.gaji < 5000000)
+    {
+        cout<<"Pegawai dibawah UMR"<<endl;
+    }else{
+        cout<<"Pegawai diatas UMR"<<endl;
+    }
+    
+}
+
+int main()
+{
+    Pegawai pgw;
+
+    cout<<"Masukan nama: ";
+    cin>>pgw.nama;
+    cout<<"Masukan jabatan: ";
+    cin>>pgw.jabatan;
+    cout<<"Masukan gaji: ";
+    cin>>pgw.gaji;
+
+    cout<<"=== Data Pegawai ==="<<endl;
+    cout<<"Nama :"<<pgw.nama<<endl;
+    cout<<"Jabatan :"<<pgw.jabatan<<endl;
+    cout<<"Gaji :"<<pgw.gaji<<endl;
+
+    umr(pgw);
+    return 0;
+}
