26b76e4f31639f227f5d32806a7633748220680b
NakhalanAtqiya <nakhalanatqiya@gmail.com>
2026-01-10 20:59:09 +0700

    quiz 1

---
+++ pertemuan_13/quiz/quiz1.cpp
@@ -0,0 +1,22 @@
+#include <iostream>
+using namespace std;
+
+int main()
+{
+    string data[3][2] ={
+    {"Dudan", "252310043"},
+    {"Maya", "252310021"},
+    {"Andi", "252310023"}
+    };
+    cout<<"Nama   NPM"<<endl;
+    for (int i = 0; i < 3; i++)
+    {
+        
+        for (int j = 0; j < 2; j++)
+        {
+            cout<<data[i][j]<<" ";
+        }
+        cout<<endl;
+    }
+    return 0;
+}
